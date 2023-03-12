2ffaca68ab463e6f52b6296dd1085d112cd43fa8
Viktor Hultman <vhultman@invidi.com>
2023-03-12 15:46:23 +0100

    Initial commit, added website code from Viktors account

---
+++ index.js
@@ -0,0 +1,34 @@
+function toSite(string) {
+  let link = `./${string}`;
+  window.location.href = link;
+}
+
+const startLink = document.querySelector(".startsida-nav-item");
+
+let menu = document.querySelector(".hamburger-menu");
+let burger = document.querySelector(".hamburger");
+let closeMenu = document.querySelector(".menu-exit");
+
+burger.addEventListener("click", function () {
+  if (menu.classList.contains("reset-animation")) {
+    menu.classList.remove("reset-animation");
+  }
+
+  menu.classList.add("dropdown-animation");
+  setTimeout(() => {
+    burger.classList.add("hide");
+    closeMenu.classList.add("show");
+  }, 100);
+});
+
+closeMenu.addEventListener("click", function () {
+  if (menu.classList.contains("dropdown-animation")) {
+    menu.classList.remove("dropdown-animation");
+  }
+
+  menu.classList.add("reset-animation");
+  setTimeout(() => {
+    burger.classList.remove("hide");
+    closeMenu.classList.remove("show");
+  }, 100);
+});
